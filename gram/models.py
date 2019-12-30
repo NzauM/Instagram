c6ad6c06a076e9b93fcc53d6fc71312ea4b0ad8d
NzauM <mercywaenu16@gmail.com>
2019-12-30 09:19:37 +0300

    Add image model functions for saving and getting images

--- gram/models.py
+++ gram/models.py
@@ -1,15 +1,39 @@
 from django.db import models
 
+
+
 # Create your models here.
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='pictures/')
     bio = models.CharField(max_length=300)
+    username = models.CharField(max_length=50,default='Your username')
+
+    def __str__(self):
+        return self.username
 
 class Image(models.Model):
-    Image = models.ImageField(upload_to = 'pictures/')
+    ig_pic = models.ImageField(upload_to= 'pictures/')
     name = models.CharField(max_length=50)
     caption = models.CharField(max_length=300)
     user = models.ForeignKey(Profile,on_delete = models.CASCADE)
     
 
+    def save_image(self):
+        self.save()
 
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def all_images(cls):
+        timeline_pics = cls.objects.all()
+        return timeline_pics
+
+    @classmethod
+    def user_pics(cls,user):
+        user_pic = cls.objects.filter(user = user)
+        return user_pic
+
+
+
+    
