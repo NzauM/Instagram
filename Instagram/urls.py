08967e6754ef015620155bd99ec5f3b541655a9d
NzauM <mercywaenu16@gmail.com>
2019-12-30 09:20:21 +0300

    Add url patterns for accesing different pages

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -18,5 +18,6 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('gram.urls'))
+    url(r'',include('gram.urls')),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
 ]
