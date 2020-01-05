74d407842fc93e4eb9232a60763d3998d215536a
NzauM <mercywaenu16@gmail.com>
2020-01-06 00:44:53 +0300

    Add a comments class

--- gram/models.py
+++ gram/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 
+
 # Create your models here.
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='pictures/')
@@ -47,9 +48,10 @@ class Image(models.Model):
 
     @classmethod
     def one_pic(cls,id):
-        one_pic = cls.objects.filter(id = ig_pic_id)
+        one_pic = cls.objects.filter(id = id)
         return one_pic
 
+    
     @classmethod
     def search_image(cls,search_term):
         searched_image = cls.objects.filter(name_icontains=search_term)
@@ -59,6 +61,26 @@ class Image(models.Model):
     @property
     def all_likes(self):
         return self.likes.count()
+
+
+class Comments(models.Model):
+    ig_pic_id = models.ForeignKey(Image,on_delete=models.CASCADE)
+    text = models.CharField(max_length=1500)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
+
+    @classmethod
+    def get_all_comments(cls,id):
+        comments = cls.objects.filter(ig_pic_id=id)
+        return comments
+
+    def save_comments(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
 
 
 
