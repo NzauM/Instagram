b11cdfe92904ec21a99a807b6aa378c21616c884
NzauM <mercywaenu16@gmail.com>
2020-01-05 17:14:47 +0300

    Add a signup view function for sending welcome emails

--- gram/views.py
+++ gram/views.py
@@ -4,7 +4,7 @@ from .models import Image,Profile
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
-from .forms import NewPostForm
+from .forms import NewPostForm,SignUpForm
 
 
 
@@ -17,7 +17,7 @@ def timeline(request):
     
 
 def like(request):
-    image = get_object_or_404(Image,id=request.POST.get(str('ig_pic.id')))
+    image = get_object_or_404(Image,id=request.POST.get('ig_pic_id'))
     user = request.User
     image.likes.add(user)
     return (redirect,'timeline')
@@ -37,3 +37,19 @@ def new_post(request):
         form = NewPostForm()
         
     return render(request,'new_post.html',{'form':form})
+
+def signUp(request):
+    if request.method=='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            name=form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            send=welcome_email(name,email)
+            HttpResponseRedirect('timeline')
+
+    else:
+        form = SignUpForm()
+
+    return render(request,'registration/registration_form.html',{'form':form})
+
