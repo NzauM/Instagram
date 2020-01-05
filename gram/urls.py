db257e0e57ee6e4cd82bc9736919709e892c6579
NzauM <mercywaenu16@gmail.com>
2020-01-05 16:32:50 +0300

    add a new url path for a new post

--- gram/urls.py
+++ gram/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.timeline,name = 'timeline')
+    url(r'^$',views.timeline,name = 'timeline'),
+    url(r'^like/$',views.like,name='like'),
+    url(r'^newpost/$',views.new_post,name='newpost'),
 ]
+
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
