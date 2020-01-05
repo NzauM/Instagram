8cdba13d5cd25c97115a5a992eadb88088c042a7
NzauM <mercywaenu16@gmail.com>
2020-01-06 00:44:23 +0300

    Add a comment form class for commenting on images

--- gram/forms.py
+++ gram/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Image,Profile
+from .models import Image,Profile,Comments
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -19,3 +19,8 @@ class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('profile_pic','bio','username')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['user','ig_pic_id']
